77eb55096f3a7c8d9b8a99293aa99cd5d27c0a36
DhruvPatel077-YU <122406263+DhruvPatel077-YU@users.noreply.github.com>
2024-03-31 12:37:30 -0400

    Update Application.java

--- Spring-Boot-Sample/src/main/java/com/example/springboot/Application.java
+++ Spring-Boot-Sample/src/main/java/com/example/springboot/Application.java
@@ -10,7 +10,7 @@ public class Application {
 	public static void main(String[] args) {
 		ApplicationContext ctx = SpringApplication.run(Application.class, args);
 
-		System.out.println("Welcome to your Sample SpringBoot app");
+		System.out.println("This is sample application for Ideal Software company.");
 
 	}
 
